457a526dc37177787d680811a93c31cc40248ee5
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2020-09-21 19:06:00 +0900

    add new plugins

--- src/Client/plugins/VolumeSetter.js
+++ src/Client/plugins/VolumeSetter.js
@@ -1,13 +1,52 @@
 import PluginBase from './pluginBase';
+import { enableAreas } from '../Constants';
 
 class VolumeSetter extends PluginBase {
-	constructor(client) {
+	constructor (client) {
 		super({
 			id: 'VolumeSetter',
-			name: Math.floor(Math.random() * 15) === 1 ? '怨霊調節' : '音量調節',
-			description: '',
-			enableArea:''
+			name: Math.floor(Math.random() * 12) === 1 ? '怨霊調節' : '音量調節',
+			description: '鼓膜の予備が要らなくなると思います',
+			version: '0.1.0',
+			author: 'tsutoringo',
+			client,
+			enableArea: [
+				enableAreas.MOVIE,
+				({ hostname }) => /.*tokyo-shoseki\.co\.jp/.test(hostname)
+			],
+			defaultConfig: {
+				enable: false,
+				otherConfigs: {
+					volume: {
+						label: '音量',
+						value: 0.1,
+						hidden: true
+					}
+				}
+			}
 		});
+	}
+
+	getVideoELement () {
+		return document.getElementById('video01') || document.getElementById('movie');
+	}
+
+	onEnable () {
+		const video01 = this.getVideoELement();
+		if (video01) {
+			this.volumeSetter();
+			const _self = this;
+			video01.addEventListener('volumechange', function () {
+				_self.config.otherConfigs.volume.value = this.volume;
+				_self._client.saveConfig();
+			});
+		}
+		super.onEnable();
+	}
+
+	volumeSetter () {
+		const vidoe = this.getVideoELement();
+		vidoe.volume = this.config.otherConfigs.volume.value >= 0 ? this.config.otherConfigs.volume.value : 0.1;
 	}
 }
 
--- src/Client/plugins/index.js
+++ src/Client/plugins/index.js
@@ -1,7 +1,11 @@
 import CharCounter from './CharCounter';
 import ToggleVideo from './ToggleVideo';
+import VolumeSetter from './VolumeSetter';
+import Theme from './Theme';
 
 export default [
 	CharCounter,
-	ToggleVideo
+	ToggleVideo,
+	VolumeSetter,
+	Theme
 ];
